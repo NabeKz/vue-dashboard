7c1036041577be50ab91de506ebd8d5443b9ef02
NabeKz <ayuzak4me38@gmail.com>
2023-12-23 11:34:24 +0900

    first commit

---
+++ src/components/box/space-box.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+type Space<T extends string = string> = `${T}px` | `${T}%`
+const { h, w } = defineProps<{ h?: Space, w?: Space }>()
+const width = w ? w : undefined;
+const height = h ? h : undefined;
+</script>
+<template>
+  <div class="space-box" :style="{ width, height }">
+  </div>
+</template>
+
+<style scoped>
+.space-box {}
+</style>
