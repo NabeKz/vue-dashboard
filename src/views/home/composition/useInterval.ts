3dbbbb0bc5759cc2c849d80d39c029d9af9a4594
NabeKz <ayuzak4me38@gmail.com>
2023-12-30 23:05:56 +0900

    add interval

---
+++ src/views/home/composition/useInterval.ts
@@ -0,0 +1,36 @@
+type Options = {
+  interval?: number
+  timeOut?: number
+}
+type Callback = {
+  timeIn: () => void
+  timeOut: () => void
+}
+const DEFAULT_INTERVAL = 1000
+const DEFAULT_TIMEOUT = 1000
+
+export const useInterval = (callback: Callback, options: Options = {}) => {
+  const timer = Array<NodeJS.Timeout>()
+
+  const resetTimer = () => {
+    const timerId = timer.pop()
+    clearTimeout(timerId)
+  }
+
+  const startTimer = () => {
+    const timeout = options.timeOut ?? DEFAULT_TIMEOUT
+    const interval = options.timeOut ?? DEFAULT_INTERVAL
+
+    setInterval(() => {
+      callback.timeIn()
+      if (timer.length > 0) return
+      const timerId = setTimeout(() => callback.timeOut(), timeout)
+      timer.push(timerId)
+    }, interval)
+  }
+
+  return {
+    resetTimer,
+    startTimer
+  }
+}
