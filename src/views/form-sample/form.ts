17fab130d1a37a9dd2425c25478c3b63ba21eb25
NabeKz <ayuzak4me38@gmail.com>
2024-01-27 16:59:39 +0900

    zodのwrapper適用

--- src/views/form-sample/form.ts
+++ src/views/form-sample/form.ts
@@ -2,8 +2,8 @@ import { z } from "@/views/_shared_/custom-validate"
 import { useCustomForm } from "@/views/_shared_/use-custom-form"
 
 const schema = z.object({
-  email: z.string().max(2).value,
-  item: z.string().value,
+  email: z.string.required().max(2),
+  item: z.string.required(),
 })
 
 export const useMyForm = () => {
