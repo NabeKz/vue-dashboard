87e43ade59e13efabaa7e2ce1b740768d9e210bd
NabeKz <ayuzak4me38@gmail.com>
2024-01-27 17:19:10 +0900

    storybookにprettier適用

--- src/views/home/components/add-modal/add-modal.stories.ts
+++ src/views/home/components/add-modal/add-modal.stories.ts
@@ -1,11 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/vue3"
-import { within, userEvent, expect } from "@storybook/test"
 import AddModal from "./AddModal.vue"
 
 const meta = {
   title: "Home/AddModal",
   component: AddModal,
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 } satisfies Meta<typeof AddModal>
 
 export default meta
@@ -13,7 +12,7 @@ type Story = StoryObj<typeof meta>
 
 // More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const Normal: Story = {
-  args: {}
+  args: {},
   // play: async ({ canvasElement }: any) => {
   //   const canvas = within(canvasElement)
   //   const loginButton = canvas.getByRole("button", { name: /Log in/i })
