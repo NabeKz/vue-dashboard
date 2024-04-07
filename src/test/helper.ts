9f646bee052651a6b1af06923ca8fd86a2e96f67
NabeKz <ayuzak4me38@gmail.com>
2024-04-07 19:51:26 +0900

    title wip

--- src/test/helper.ts
+++ src/test/helper.ts
@@ -55,6 +55,6 @@ export const protectedLayout = () => () => ({
 })
 
 export const getErrorField = (el: HTMLElement) => {
-  console.debug("test WIP push")
+  console.debug("title wip")
   return el.nextSibling!
 }
